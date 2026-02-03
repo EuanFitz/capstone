8992bb649420555a8040d295c82918468799743e
EuanFitz <euan.fitzpatty@gmail.com>
2026-02-03 14:33:59 -0700

    Styling for user intake

--- user.php
+++ user.php
@@ -30,7 +30,7 @@ if(isset($_GET['company'])){
 //Queries
     $newemployeequery= "INSERT INTO `employee` (`employee_id`, `employee_first`,`employee_last`, `employee_email`, `employee_phone`, `company_id`, `department_id`) VALUES (NULL, '$emp_first', '$emp_last', '$emp_email', NULLIF($emp_phone,0), '2', '2')";
 
-    print_r($newemployeequery);
+    // print_r($newemployeequery);
     }
 
     $companyquery = "SELECT * FROM company c 
@@ -43,67 +43,81 @@ if(isset($_GET['company'])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User intake || ClickSafe.com</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    <?php
-        if(isset($_POST['submit'])){
-            mysqli_query($connection, $newemployeequery);
+    <main>
+        <?php
+            if(isset($_POST['submit'])){
+                mysqli_query($connection, $newemployeequery);
+            ?>
+            <section>
+                <h3>Completed!</h3>
+                <p>Thankyou for filling out the form.</p>
+                <p>You will receive a confirmation email, with a link to confirm your account.</p>
+            </section>
+            <section>
+                <img src="logo" alt="ClickSafe">
+                <h1>ClickSafe</h1>
+            </section>
+            <?php
+            }else{
         ?>
         <section>
-            <h3>Completed!</h3>
-            <p>Thankyou for filling out the form.</p>
-            <p>You will receive a confirmation email, with a link to confirm your account.</p>
+            <h2>Employee Regristration</h2>
+                <form action="user.php" method="post"> 
+                    <div>
+                        <label for="first">First name</label>
+                        <input type="text" name="first" id="first" placeholder="First name" required>
+                    </div>
+                    <div>
+                        <label for="last">Last name</label>
+                        <input type="text" name="last" id="last" placeholder="Last name" required>
+                    </div>
+                    <div>
+                        <!-- This will be based on companies selected departments -->
+                        <label for="department">Department</label>
+                        <select name="department" id="department" required>
+                            <option value="">Select Department</option>
+                            <option value="">1</option>
+                            <option value="">2</option>
+                            <option value="">3</option>
+                            <option value="">4</option>
+                        </select>
+                    </div>
+                    <div>
+                        <!-- This will be based on companies selected departments -->
+                        <label for="department">Company</label>
+                        <select name="company" id="company" required>
+                            <option value="">Select Department</option>
+                            <option value="">1</option>
+                            <option value="">2</option>
+                            <option value="">3</option>
+                            <option value="">4</option>
+                        </select>
+                    </div>
+                    <div>
+                        <label for="email">Email</label>
+                        <input type="email" name="email" id="email" placeholder="Email" required>
+                    </div>
+                    <div>
+                        <label for="phone">Phone</label>
+                        <input type="tel" name="phone" id="phone" placeholder="Phone">
+                    </div>
+
+                        <button type="submit" name="submit">Sign up</button>
+                </form>
         </section>
         <section>
-            <img src="logo" alt="ClickSafe">
-            <h1>ClickSafe</h1>
+            <img src="logo" alt="ClickSafe logo">
+            <h2>Welcome to ClickSafe</h2>
+            <p>This form will link your email and work numbers to your Admin or IT team. This will allow them to send phishing simulations and training information to you throughout the year. Please ensure all your details are correct before submitting. Afterwards, you will not be required to go back to this site.</p>
+
+            <p>Thank you for using ClickSafe!</p>
         </section>
         <?php
-        }else{
-    ?>
-    <section>
-            <form action="user.php" method="post"> 
-                <div>
-                    <label for="first">First name</label>
-                    <input type="text" name="first" id="first" placeholder="First name" required>
-                </div>
-                <div>
-                    <label for="last">Last name</label>
-                    <input type="text" name="last" id="last" placeholder="Last name" required>
-                </div>
-                <div>
-                    <label for="email">Email</label>
-                    <input type="email" name="email" id="email" placeholder="Email" required>
-                </div>
-                <div>
-                    <label for="phone">Phone</label>
-                    <input type="tel" name="phone" id="phone" placeholder="Phone">
-                </div>
-                <div>
-
-                    <!-- This will be based on companies selected departments -->
-                    <label for="department">Department</label>
-                    <select name="department" id="department" required>
-                        <option value="">Select Department</option>
-                        <option value="">1</option>
-                        <option value="">2</option>
-                        <option value="">3</option>
-                        <option value="">4</option>
-                    </select>
-
-                    <button type="submit" name="submit">Sign up</button>
-                </div>
-            </form>
-    </section>
-    <section>
-        <img src="logo" alt="ClickSafe logo">
-        <h2>Welcome to ClickSafe</h2>
-        <p>This form will link your email and work numbers to your Admin or IT team. This will allow them to send phishing simulations and training information to you throughout the year. Please ensure all your details are correct before submitting. Afterwards, you will not be required to go back to this site.</p>
-
-        <p>Thank you for using ClickSafe!</p>
-    </section>
-    <?php
-        }
-    ?>
+            }
+        ?>
+    </main>
 </body>
 </html>
