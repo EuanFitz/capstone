f80a2f39e10fd3f670b417de8586eda58ea1f1e5
EuanFitz <euan.fitzpatty@gmail.com>
2026-02-27 16:16:54 -0700

    making images how up on dashboard

--- public/app.js
+++ public/app.js
@@ -1,6 +1,16 @@
+
 async function getGraphs(){
+    const container = document.querySelector('#images');
+
         const res = await fetch('/graphs');
         const data = await res.json();
         console.log(data);
+        
+        data.forEach(chart =>{
+            const img = document.createElement('img');
+            img.src = `/${chart.filename}`;
+            img.alt = chart.alt;
+            container.appendChild(img);
+        });
 }
 getGraphs();
