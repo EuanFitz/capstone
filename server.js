6a2e273cc5c0bcf7b30e937580d9e7ce6d4ded73
EuanFitz <euan.fitzpatty@gmail.com>
2026-03-22 18:09:48 -0600

    Just some cleaning up

--- server.js
+++ server.js
@@ -8,9 +8,6 @@ const helmet = require('helmet');
 const path = require('path');
 const mongoose = require('mongoose');
 const app = express();
-const session = require('express-session');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const PORT_HTTPS = process.env.PORT || 3443; 
 
@@ -44,23 +41,6 @@ app.use(helmet({
 app.set("view engine", "ejs"); //
 app.set("views", path.join(__dirname, "views"));
 
-// //-----------------------Static cacheing-------------------
-// app.use(express.static('public', {
-//     setHeaders: (res, path) => {
-//         if (path.endsWith('.css')) {
-//             res.set('Cache-Control', 'public, max-age=86400, immutable'); // Cache for 24 hours
-//         }
-//         if (path.endsWith('.jpg') || path.endsWith('.png')) {
-//             res.set('Cache-Control', 'public, max-age=2592000, immutable'); // Cache images for 30 days
-//         }
-//     }
-// }));
-    
-
-//---------------------templates-----------------
-// app.set("view engine", "ejs");
-// app.set("views", path.join(__dirname, "views"));
-// look into ejs and making "empty" html pages that hold includes from ejs
 
 //-----------------------------Route------------------------
 
@@ -105,54 +85,6 @@ async function connectDB() {
     }
 }
 
-// const graphs = [
-//         {id: 1, filename: "Dashboard-piechart.png", alt: "A purple piechart"},
-//         {id: 2, filename: "Dashboard-bargraph.png", alt: "a purple bar graph"},
-//         {id: 3, filename: "Dashboard-linegraph.png", alt: "a purple line graph"},
-//         {id: 4, filename: "Dashboard-percentage.png", alt: "a circular representation of a percentage"},
-//         {id: 5, filename: "Dashboard-circlegraph.png", alt: "a circle graph"}
-//     ];
-
-// app.get("/graphs", (req,res)=>{
-//     res.set('Cache-Control', 'max-age=300, stale-while-revalidate=60');
-//     res.json(graphs);
-// });
-
-// app.get("/graphs/:id", (req, res) => {
-
-//     const id = parseInt(req.params.id);
-
-//     const graph = graphs.find(c => c.id === id);
-
-//     if (!graph) {
-//         return res.status(404).send("Not found");
-//     }
-
-//     res.set("Cache-Control", "public, max-age=300");
-
-//     res.json(graph);
-// });
-
-
-//------------------------DATABASE CONNECT/QUERY------------
-// const connection = require('connection.js');
-
-// connection.connect(function(err) {
-//   if (err) throw err;
-//   console.log("Connected!");
-
-// //--------------------VVV QUIRIES HERE VVV---------------------------
-
-//   connection.query('',function (error, results, feilds){
-//     if (error) throw error;
-//   })
-
-//   connection.end(); 
-// });
-// //------------------------ERROR HANDLING---------------------
-// app.use((req, res) =>{
-//     res.status(404).render("pages/404", { title: "Not found"});
-// });
 
 
 //-------------------------Key & Cert------------------------
