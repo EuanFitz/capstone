6c43d5258fa3156c39e46f5d8df06d8c853405d1
EuanFitz <euan.fitzpatty@gmail.com>
2026-03-22 18:20:10 -0600

    Update passport.js

--- routes/passport.js
+++ routes/passport.js
@@ -6,7 +6,7 @@ const User = require('../model/User');
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "https:/localhost:3456/api/auth/google/callback"
+    callbackURL: "/api/auth/google/callback"
 }, async(accessToken, refreshToken, profile, done) =>{
     try{
         // first see if the google user id is in the database
