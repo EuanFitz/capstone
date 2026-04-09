b871ef30b734593fcfa90db763615ee95c4259b0
EuanFitz <euan.fitzpatty@gmail.com>
2026-04-08 22:07:32 -0600

    blah
    just got rid of a few console.logs i set for debugging

--- middleware/auth.js
+++ middleware/auth.js
@@ -45,8 +45,6 @@ const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser"); 
  
 const authMiddleware = (req, res, next) => {
-  console.log("authMiddleware hit");
-  console.log("cookies:", req.cookies);
   
   // Checks cookie first, then uses security header if doesn't work/no cookie/ other issues etc. 
   const token = req.cookies?.token || 
